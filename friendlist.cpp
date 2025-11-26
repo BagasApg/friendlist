6e31d1e83775fa08cf5aa6da7d40b3db3cfa375e
BagasApg <bagasapg@gmail.com>
2025-11-26 19:34:38 +0700

    Rebuilt successfully. Added height count to the tree. TODO: Implement balance factor and add rotation functions to complete AVL

--- friendlist.cpp
+++ friendlist.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-// #include <sstream>
+#include <sstream>
 
 struct Node
 {
-    int id, level, height;
-    std::string name, rank;
+    int id, level;
+    std::string username, rank;
 
+    int height;
     Node *left;
     Node *right;
 };
@@ -16,7 +17,7 @@ Node *createNewNode(int id, int level, std::string name, std::string rank)
 
     newNode->id = id;
     newNode->level = level;
-    newNode->name = name;
+    newNode->username = name;
     newNode->rank = rank;
     newNode->height = 1;
 
@@ -24,13 +25,13 @@ Node *createNewNode(int id, int level, std::string name, std::string rank)
     return newNode;
 }
 
-Node *findMin(Node *root)
+int height(Node *node)
 {
-    while (root->left != nullptr)
+    if (node == nullptr)
     {
-        root = root->left;
+        return 0;
     }
-    return root;
+    return node->height;
 }
 
 int max(int left, int right)
@@ -39,237 +40,122 @@ int max(int left, int right)
     {
         return left;
     }
-    else
-    {
-        return right;
-    }
+    return right;
 }
 
-int height(Node *root)
+Node *insertNode(Node *root, int id, int level, std::string username, std::string rank)
 {
     if (root == nullptr)
     {
-        return -1;
+        root = createNewNode(id, level, username, rank);
+    }
+
+    else if (id <= root->id)
+    {
+        root->left = insertNode(root->left, id, level, username, rank);
     }
     else
     {
-        root->height = max(height(root->left), height(root->right)) + 1;
+        root->right = insertNode(root->right, id, level, username, rank);
     }
-    return root->height;
-}
 
-Node *rightRotate(Node *root)
-{
-    Node *temp = root->left;
-    root->left = temp->right;
-    temp->right = root;
+    root->height = 1 + max(height(root->left), height(root->right));
+    std::cout << root->height << "-" << root->username << std::endl;
 
-    root->height = height(root);
-    temp->height = height(temp);
-    return temp;
-}
-
-Node *leftRotate(Node *root)
-{
-    Node *temp = root->right;
-    root->right = temp->left;
-    temp->left = root;
-
-    root->height = height(root);
-    temp->height = height(temp);
-    return temp;
-}
-
-int getBalance(Node *root)
-{
-    if (root == nullptr)
-    {
-        return 0;
-    }
-    else
-    {
-        return height(root->left) - height(root->right);
-    }
-}
-
-Node *rebalance(Node *root)
-{
-    root->height = height(root);
-    if (getBalance(root) == 2)
-    {
-        if (getBalance(root->right) < 0)
-        {
-            root->right = rightRotate(root->right);
-        }
-        return leftRotate(root);
-    }
-    if (getBalance(root) == -2)
-    {
-        if (getBalance(root->left) > 0)
-        {
-            root->left = leftRotate(root->left);
-        }
-        return rightRotate(root);
-    }
     return root;
 }
 
-Node *insertNode(Node *root, int id, int level, std::string name, std::string rank)
+std::string rank(std::string r)
 {
-    if (root == nullptr)
+    std::string res = "";
+    std::string buffer;
+
+    std::istringstream ss(r);
+
+    std::getline(ss, buffer, '-');
+    if (buffer == "0")
     {
-        root = createNewNode(id, level, name, rank);
+        res += "Warrior";
     }
-    else if (id < root->id)
+    else if (buffer == "1")
     {
-        root->left = insertNode(root->left, id, level, name, rank);
+        res += "Elite";
+    }
+    else if (buffer == "2")
+    {
+        res += "Master";
+    }
+    else if (buffer == "3")
+    {
+        res += "Grandmaster";
+    }
+    else if (buffer == "4")
+    {
+        res += "Epic";
+    }
+    else if (buffer == "5")
+    {
+        res += "Legend";
+    }
+    else if (buffer == "6")
+    {
+        res += "Mythic";
+    }
+    else if (buffer == "7")
+    {
+        res += "Mythical Honor";
+    }
+    else if (buffer == "8")
+    {
+        res += "Mythical Glory";
     }
     else
     {
-        root->right = insertNode(root->right, id, level, name, rank);
+        res += "Mythical Immortal";
     }
-    return rebalance(root);
+
+    std::getline(ss, buffer, '-');
+
+    return res + " " + buffer;
 }
 
-Node *search(Node *root, int id)
+void show(Node *root, int step = 0)
 {
     if (root == nullptr)
     {
-        return nullptr;
-    }
-    else if (id == root->id)
-    {
-        return root;
-    }
-    else if (id <= root->id)
-    {
-        return search(root->left, id);
-    }
-    else
-    {
-        return search(root->right, id);
-    }
-}
-
-Node *deleteNode(Node *root, int id)
-{
-    if (root == nullptr)
-    {
-        return nullptr;
-    }
-    else if (id == root->id)
-    {
-        if (root->left == nullptr)
+        if (step == 0)
         {
-            Node *temp = root->right;
-            delete root;
-            return temp;
+            std::cout << "You have no friend :(";
         }
-        else if (root->right == nullptr)
-        {
-            Node *temp = root->left;
-            delete root;
-            return temp;
-        }
-        Node *temp = findMin(root->right);
-        root->id = temp->id;
-        root->right = deleteNode(root->right, temp->id);
-    }
-    else if (id < root->id)
-    {
-        root->left = deleteNode(root->left, id);
-    }
-    else
-    {
-        root->right = deleteNode(root->right, id);
-    }
-    return rebalance(root);
-}
-
-void show(Node *root)
-{
-    if (root == nullptr)
-    {
         return;
     }
 
-    show(root->left);
-    std::cout << root->id << "\t" << root->name << "\t\t" << root->level << "\t" << root->rank << std::endl;
-    show(root->right);
+    show(root->left, ++step);
+
+    std::cout << root->height << "\t";
+    std::cout << root->id << "\t";
+    std::cout << root->level << "\t";
+    std::cout << root->username << "\t\t";
+    std::cout << rank(root->rank) << "\n";
+
+    show(root->right, step++);
 }
-
-// std::string rank(std::string r)
-// {
-//     std::string res = "";
-//     std::string buffer;
-
-//     std::istringstream ss(r);
-
-//     std::getline(ss, buffer, '-');
-//     if (buffer == "0")
-//     {
-//         res += "Warrior";
-//     }
-//     else if (buffer == "1")
-//     {
-//         res += "Elite";
-//     }
-//     else if (buffer == "2")
-//     {
-//         res += "Master";
-//     }
-//     else if (buffer == "3")
-//     {
-//         res += "Grandmaster";
-//     }
-//     else if (buffer == "4")
-//     {
-//         res += "Epic";
-//     }
-//     else if (buffer == "5")
-//     {
-//         res += "Legend";
-//     }
-//     else if (buffer == "6")
-//     {
-//         res += "Mythic";
-//     }
-//     else if (buffer == "7")
-//     {
-//         res += "Mythical Honor";
-//     }
-//     else if (buffer == "8")
-//     {
-//         res += "Mythical Glory";
-//     }
-//     else
-//     {
-//         res += "Mythical Immortal";
-//     }
-
-//     std::getline(ss, buffer, '-');
-
-//     return res + " " + buffer;
-// }
 
 main()
 {
 
     Node *root = nullptr;
 
-    root = insertNode(root, 15, 15, "TanteiBozu", "4-1");
-    root = insertNode(root, 20, 15, "Celestia.", "4-2");
-    std::cout << "yok";
-    root = insertNode(root, 30, 15, "impi", "4-3");
-    root = insertNode(root, 5, 15, "Epylepsee", "4-4");
-
-    Node *res = search(root, 5);
+    root = insertNode(root, 1, 33, "suichan", "3-1");
+    std::cout << std::endl;
+    root = insertNode(root, 3, 33, "Vretz", "3-1");
+    std::cout << std::endl;
+    root = insertNode(root, 2, 33, "bunnyhop", "3-1");
+    std::cout << std::endl;
 
     show(root);
-    deleteNode(root, 15);
-    show(root);
 
-    // std::cout << (res->rank);
-
+    std::cout << std::endl
+              << "Done!";
     return 0;
 }
